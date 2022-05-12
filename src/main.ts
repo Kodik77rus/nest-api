5ecd2482513b2b5974688a3978b63f53e691d1db
Kodik77rus <plyhach@gmail.com>
2022-05-12 07:18:56 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
