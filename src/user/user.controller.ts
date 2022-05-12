2b59e0ac6b3b61948b7178e1fcc6fa077331e760
Kodik77rus <plyhach@gmail.com>
2022-05-12 07:31:03 +0300

    add jwt guard to user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -6,30 +6,38 @@ import {
   Param,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
+
+import { UserService } from './user.service';
+import { JwtAuthGuard } from '../auth/jwt-auth-guard';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly service: UserService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get(':name')
   async find(@Param('name') userId: string) {
     return await this.service.find(userId);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   async create(@Body() user: CreateUserDto) {
     return await this.service.create(user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':name')
   async update(@Param('name') id: string, @Body() user: UpdateUserDto) {
     return await this.service.update(id, user);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':name')
   async delete(@Param('name') name: string) {
     return await this.service.delete(name);
